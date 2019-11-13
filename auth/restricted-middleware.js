69eec392256d0a7414ddf75ad56a01e0da7a6cd9
Kevin Allen <kevin.allen91@gmail.com>
2019-11-13 10:08:34 -0800

    added middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,24 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model.js");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    const secret = process.env.JWT_SECRET || "keep it secret";
+
+    // check that token is valid
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (err) {
+        //bad token, token has been tampered with
+        res.status(401).json({ message: `bad token` });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "No credentials provided" });
   }
 };
--- users/users-router.js
+++ users/users-router.js
@@ -3,12 +3,22 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, checkRole(['student','admin']) (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function checkRole(roles) {
+  return function(req, res, next) {
+    if (roles.includes(req.decodedJwt.role)){
+      next();
+    } else {
+        res.status(403).json({message: `unauthorized access`})
+    }
+  }
+}
 
 module.exports = router;
